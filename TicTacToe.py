c45314b6b74ebc2f1e7e595dc7089af222f18aee
Suren VB <surenz298@gmail.com>
2016-09-29 08:05:29 -0400

    Added to checks for Tie

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,10 +4,13 @@ import sys
 Board = {'top-L': '', 'top-M': '', 'top-R': '',
          'mid-L': '', 'mid-M': '', 'mid-R': '',
          'low-L': '', 'low-M': '', 'low-R': ''}
-
+# Use for identying the moves
 GameBoard = {'top-L': 7, 'top-M': 8, 'top-R': 9,
              'mid-L': 4, 'mid-M': 5, 'mid-R': 6,
              'low-L': 1, 'low-M': 2, 'low-R': 3}
+
+winning_combination = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
 
 # Print out a board and associate values
 def printBoard(board: object) -> object:
@@ -19,34 +22,71 @@ def printBoard(board: object) -> object:
 
 
 # A Function for update the board based on the userInput
+# Parameters:
+# inp - the Move the user types in
+# Board - the Board on which the game is played
+# user - Which user turn - based on X or O
 def updateBoard(inp, board, user):
     check = board.get(inp, "None")
     if check == "None":
         return
     elif check == symbolInput or check == systemPlaySymbol:
-        print("That spot is already taken - Choose another one")
-        return
+        #print("That spot is already taken - Choose another one")
+        return 'X'
     else:
         board[inp] = user
 
-def winboard():
-    return "None"
+# AI part of the game
+
+def winboard(s):
+    return
+
+'''
+def minimax(node, player):
+    if node.complete():
+        if 'W' == calculateWinner(symbolInput):#node.X_won():
+            return -1
+        elif 'T' == :
+            return 0
+        elif node.O_won():
+            return 1
+        best = None
+    for move in node.available_moves():
+        node.make_move(move, player)
+        val = self.minimax(node, get_enemy(player), alpha, beta)
+        node.make_move(move, None)
+        if player == 'O':
+            if val > best:
+                best = val
+        else:
+            if val < best:
+                best = val
+        return best
+'''
+# A function to check for a Winner after every Move
+# Parameters:
+# symbol - X or O
+# Pre-set the Winning combination
+# Pick the values from GameBoard based on the User Move
+# Check if the Players has made any of the winning moves
+# if so return a value - if not return nothing
 
 def calculateWinner(symbol):
     checkList = []
     win =''
     winning_combination = [[7, 8, 9], [4, 6, 5], [1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+
     for key, values in Board.items():
         if values == symbol:
             checkList.append(GameBoard[key])
-    #for i in len(winning_combination)
     for i in range(0, len(winning_combination)):
         if set(checkList) == set(winning_combination[i]):
             win = 'W'
     return win
 
-
 # Ask for input from User
+# Handle false or NO inputs
 print("Would you like to Play Tic-Tac-Toe with me?")
 userInput = input().lower()
 if not userInput:
@@ -64,6 +104,8 @@ while userInput:
 
 print('All right lets play')
 
+# Get Synbol the user wants to choose
+# The Computer Gets the next one
 print("Choose a symbol for your play --X or O?")
 symbolInput = input()
 if symbolInput == 'O':
@@ -71,9 +113,8 @@ if symbolInput == 'O':
 else:
     systemPlaySymbol = 'O'
 print("You are gonna be using " + symbolInput + " for your game" + '\n'
-                                                          "I am gonna be using " + systemPlaySymbol + " for my moves")
-
-# noinspection PyGlobalUndefined
+                                                                        "I am gonna be using " + systemPlaySymbol + " for my moves")
+# Play until a Winner
 win = ''
 printBoard(Board)
 while not win:
@@ -91,6 +132,9 @@ while not win:
     if win:
         print("You won " + symbolInput)
         break
+    if '' not in [v for v in Board.values()]:
+        print("its a tie")
+        break
     print("Next is my turn")
     comMove = input()
     #ComSpot = winboard()
@@ -98,9 +142,10 @@ while not win:
     printBoard(Board)
     win = calculateWinner(systemPlaySymbol)
     if win:
-        print("You won " + systemPlaySymbol)
+        print("You won " + symbolInput)
         break
-
-
+    if '' not in [v for v in Board.values()]:
+        print("its a tie")
+        break
 
 
